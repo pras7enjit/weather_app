2d8cec29e7f549a5f5e2e5203522009e00b7dc31
Ghosh <babu.balakrishnan@digiapt.com>
2017-05-17 17:24:05 +0530

    test push

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	/*Creater: Ghosh*/
 	// "url": "mongodb://app_weather:app_weather@ds011912.mlab.com:11912/app_weather",
 	// "name": "app_weather"
 	"cityByNameUrl": "http://api.openweathermap.org/data/2.5/forecast/daily?",
